ceab9c486e1340d41c78932fb9f61e28ae12f5fe
DariyaShagieva <dariyashagieva@gmail.com>
2017-10-04 00:33:21 +0300

    задание 11

---
+++ hw4oct/src/ru/itis/ex11c.java
@@ -0,0 +1,15 @@
+package ru.itis;
+
+import java.util.Scanner;
+
+public class ex11c {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        double p = 1;
+        for (int i = 1; i <= n; i++){
+            p *=(Math.tan(i));
+        }
+        System.out.print(p);
+    }
+}
---
+++ hw4oct/src/ru/itis/ex11d.java
@@ -0,0 +1,21 @@
+package ru.itis;
+
+import java.util.Scanner;
+
+public class ex11d {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double p = scanner.nextDouble();
+        double s = scanner.nextDouble();
+        double x = scanner.nextDouble();
+        double t = 1;
+        int n = scanner.nextInt();
+        for ( int i = 1; i <= n; i++){
+            t *=x;
+            p *= Math.cos(t);
+            s += Math.cos(t);
+        }
+        System.out.println(s);
+        System.out.print(p);
+    }
+}

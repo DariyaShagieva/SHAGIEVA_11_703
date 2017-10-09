26fc261789c393ea954d3d39ff56119024678279
DariyaShagieva <dariyashagieva@gmail.com>
2017-10-10 01:10:22 +0300

    задания 12 и 7

--- hw11oct/src/ru/itis/ex12d.java
+++ hw11oct/src/ru/itis/ex12d.java
@@ -7,7 +7,7 @@ public class ex12d {
         Scanner scanner = new Scanner(System.in);
         int p = 1, n = 0;
         double x = scanner.nextDouble();
-        double e = 0.000001, t = 1, s = 0;
+        double e = 0.0001, t = 1, s = 0;
 
         while (Math.abs(t) > e) {
             s += t * p;

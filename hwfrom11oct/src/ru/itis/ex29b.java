c8c027f90dc4c6ef546977cd9903c36f10cc2c72
DariyaShagieva <dariyashagieva@gmail.com>
2017-10-11 14:59:18 +0300

    задание 29b от 11окт

---
+++ hwfrom11oct/src/ru/itis/ex29b.java
@@ -0,0 +1,16 @@
+package ru.itis;
+
+public class ex29b {
+    public static void main(String[] args) {
+        double x0 = 0, x1, e = 0.0001;
+        int n = 1;
+        x1 = Math.cos(x0);
+        while (Math.abs(x1 - x0) > e) {
+            x0 = x1;
+            x1 = Math.cos(x0);
+            n++;
+        }
+        System.out.print(n);
+
+    }
+}

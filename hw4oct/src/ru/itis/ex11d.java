4030293c1503769535634ffd54eef3e12a264e72
DariyaShagieva <dariyashagieva@gmail.com>
2017-10-17 22:15:38 +0300

    исправл 11г 17окт

--- hw4oct/src/ru/itis/ex11d.java
+++ hw4oct/src/ru/itis/ex11d.java
@@ -3,12 +3,13 @@ package ru.itis;
 import java.util.Scanner;
 
 public class ex11d {
+    // задание 11 г
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         double p = 1;
         for (int i = 1; i <= n; i++){
-            p *=(Math.tan(i));
+            p *= Math.sin(i)/Math.cos(i);
         }
         System.out.print(p);
     }

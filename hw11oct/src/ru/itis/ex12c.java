7204b062677de7f899c6a26230719ebcc0e21653
DariyaShagieva <dariyashagieva@gmail.com>
2017-10-24 21:50:06 +0300

    исправление 12в 24окт

--- hw11oct/src/ru/itis/ex12c.java
+++ hw11oct/src/ru/itis/ex12c.java
@@ -7,14 +7,14 @@ public class ex12c {
         Scanner scanner = new Scanner(System.in);
         int n = 1, p =1;
         double x = scanner.nextDouble();
-        double e = 0.0000001, t = x, s = 0, l = x;
+        double e = 0.0000001, t = x, s = 0;
 
         while (Math.abs(t) > e){
             s += t*p;
             p = -p;
             n++;
-            l *=x;  //плохо
-            t = l/n;
+            t *= x*(n-1)/n;
+
         }
         System.out.print(s);
     }

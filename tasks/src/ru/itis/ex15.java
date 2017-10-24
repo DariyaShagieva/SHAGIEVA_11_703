83cf57b78ce42361decd48d7d9900043a464746e
DariyaShagieva <dariyashagieva@gmail.com>
2017-10-24 15:00:49 +0300

    задание 15 24окт

--- tasks/src/ru/itis/ex15.java
+++ tasks/src/ru/itis/ex15.java
@@ -7,21 +7,17 @@ public class ex15 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int m = 0, p = n;
-        int k = 0;
-        while (n != 0) {
-            k++;
-            n /= 10;
-        }
-        int[] a = new int[k];
-        for (int i = 0; i < k; i++){
-            a[i] = p%10;
-            p /= 10;
-        }
-        for ( int i = k-1; i >= 0; i--){
-            if (a[i] % 2 == 1) {
-                System.out.print(a[i]);//а где построенное число?
+        int k = 1, m = 0;
+        while (n > 0) {
+            if (n % 10 % 2 == 1 ){
+                m += n%10*k;
+                k *=10;
+
             }
+            n /=10;
+
         }
+        System.out.print(m);
     }
+
 }

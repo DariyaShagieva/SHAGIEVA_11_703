db2efd2d9265d90693ee38369e03b1cee4f75803
DariyaShagieva <dariyashagieva@gmail.com>
2017-11-15 10:31:54 +0300

    исправление 15а

--- hwfrom1nov/src/ru/itis/ex15a.java
+++ hwfrom1nov/src/ru/itis/ex15a.java
@@ -3,10 +3,10 @@ package ru.itis;//-
 public class ex15a {
     // задание 15 а
     public static void main(String[] args) {
-        int[] x = {1, 3, 3, 4, 5, 6, 2, 4, 3, 1, 3, 6};
+        int[] x = {1, 3, 3, 4, 5, 6, 2, 4, 5, 1, 3, 6, 7, 8, 9, 10, 1};
         int t = 0 , s = x.length, k = 1;
-        for ( int i = 0; i < x.length - 1; i++){
-            if(x[i] <= x[i+1]) k++;
+        for ( int i = 0; i < x.length - 1; i++) {
+            if (x[i] <= x[i + 1]) k++;
             else {
                 if (k < s) s = k;
                 if (k > t) t = k;
@@ -14,11 +14,11 @@ public class ex15a {
             }
 
         }
+        if ( k < s) s = k;
+        if ( k > t) t = s;
         System.out.println("самая короткая цепочка = " + s);
         System.out.println("самая длинная цепочка = " + t);
 
     }
 }
-//int[] x = {1, 3, 3, 4, 5, 6, 2, 4, 5, 1, 3, 6, 1};
-//самая короткая цепочка = 3
-//самая длинная цепочка = 6    это неверно
+

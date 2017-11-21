1e4029b38086391257fdeaa00e5c432187cb76f4
DariyaShagieva <dariyashagieva@gmail.com>
2017-11-21 19:40:06 +0300

    исправление

--- hwfrom1nov/src/ru/itis/ex15a.java
+++ hwfrom1nov/src/ru/itis/ex15a.java
@@ -1,4 +1,4 @@
-package ru.itis;//-
+package ru.itis;
 
 public class ex15a {
     // задание 15 а
--- hwfrom1nov/src/ru/itis/ex22d.java
+++ hwfrom1nov/src/ru/itis/ex22d.java
@@ -1,31 +1,24 @@
-package ru.itis;//-
+package ru.itis;
 
 public class ex22d
         // задание 22 д
 {
     public static void main(String[] args) {
-        int[] a = {1, 2, 3, 4, 2, 3, 6, 5 ,3 ,7, -1};
-        int k = 0;
-        for(int i = a.length-1 ; i > 0 ; i--){
-            for(int j = 0 ; j < i ; j++){
-            if( a[j] > a[j+1] ){
-                int t = a[j];
-                a[j] = a[j+1];
-                a[j+1] = t;
+        int[] a = {1, 3, 3, 4, 5, 6, 2, 4, 3, 1, 3, 6};
+        int k = 0,z = 0;
+
+
+        for( int i = 0; i < a.length ; i++) {
+            for (int j = 0; j <a.length ; j++) {
+                if (i != j) {
+                    if (a[i] != a[j]) k++;
+                }
             }
+            if (k == a.length-1) z++;
+            k = 0;
         }
-        }
-        if (a[1] != a[2]) k++;
-        if (a[a.length - 1] != a[a.length -2]) k++;
-
-
-        for( int i = 1; i < a.length - 1; i++) {
-            if (a[i] != a[i-1] && a[i] != a[i+1]) k++;
-
-        }
-        System.out.println(k);
+        System.out.println(z);
 
 
     }
 }
-//задача была на программирование кванторной формулы. В вашем решении есть один лишний проход по массиву

5d5e22644648f571ab3a8e98f0a3bf6c95063b48
DariyaShagieva <dariyashagieva@gmail.com>
2017-11-27 23:05:32 +0300

    исправление

--- hwfrom1nov/src/ru/itis/ex22d.java
+++ hwfrom1nov/src/ru/itis/ex22d.java
@@ -1,23 +1,24 @@
-package ru.itis;//-
+package ru.itis;
 
 public class ex22d
         // задание 22 д
 {
     public static void main(String[] args) {
-        int[] a = {1, 3, 3, 4, 5, 6, 2, 4, 3, 1, 3, 6};
-        int k = 0,z = 0;
+        int[] a = {1, 3, 2, 3};
+        int k = 0;
+        boolean f;
 
 
         for( int i = 0; i < a.length ; i++) {
-            for (int j = 0; j <a.length ; j++) {
+            f = true;
+            for (int j = 0; j <a.length && f ; j++) {
                 if (i != j) {
-                    if (a[i] != a[j]) k++;
+                    if (a[i] == a[j]) f = false;
                 }
             }
-            if (k == a.length-1) z++;
-            k = 0;
+            if (f) k++;
         }
-        System.out.println(z);  //bad solution
+        System.out.println(k);
 
 
     }

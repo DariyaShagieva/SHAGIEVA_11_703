2a50fed6b6e362edff9b0dba9db8f8aa5fff2833
DariyaShagieva <dariyashagieva@gmail.com>
2017-11-29 01:41:30 +0300

    add ComplexNumber

---
+++ ex51/src/ru/itis/Main.java
@@ -0,0 +1,9 @@
+package ru.itis;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    ComplexNumber number = new ComplexNumber(7 , 8);
+        System.out.println(number);
+    }
+}

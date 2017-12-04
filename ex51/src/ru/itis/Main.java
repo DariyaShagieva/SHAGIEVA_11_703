67a46c126353d99cd7e62f3a3205bf5b73fc0bf1
DariyaShagieva <32329139+DariyaShagieva@users.noreply.github.com>
2017-12-04 22:09:51 +0300

    Delete Main.java

--- ex51/src/ru/itis/Main.java
+++
@@ -1,9 +0,0 @@
-package ru.itis;
-
-public class Main {
-
-    public static void main(String[] args) {
-	    ComplexNumber number = new ComplexNumber(7 , 8);
-        System.out.println(number);
-    }
-}

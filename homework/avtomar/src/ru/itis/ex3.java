ba60d2a34796aa9d4a1b4d266752914d6ea038d4
DariyaShagieva <dariyashagieva@gmail.com>
2017-12-19 22:56:30 +0300

    add avtomat

--- homework/avtomar/src/ru/itis/ex3.java
+++ homework/avtomar/src/ru/itis/ex3.java
@@ -2,22 +2,31 @@ package ru.itis;
 
 import java.util.Scanner;
 
-public class ex1 {
+public class ex3 {
     public static boolean goodWord(String str){
         int state = 0;
-        for(int i = 0; i < str.length() && state < 2; i++){
+        boolean f = true;
+        for (int i = 0; i < str.length() && f; i++) {
             switch (state){
                 case 0:
-                    if(str.charAt(i) == '1') state++;
+                    if(str.charAt(i) != '0') f = false;
+                    else state++;
                     break;
                 case 1:
+                    if(str.charAt(i) == '1') state++;
+                    break;
+                case 2:
                     if(str.charAt(i) == '0') state++;
                     break;
+                case 3:
+                    if(str.charAt(i) == '1') f = false;
             }
+
         }
-        if (state == 2) return true;
-        else return false;
+        if (state != 3) f = false;
+        return f;
     }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String s = scanner.nextLine();
@@ -28,4 +37,5 @@ public class ex1 {
 
         }
     }
+
 }

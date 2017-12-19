0d370fb826ec9a0958d04dce4b437cf650397ed0
DariyaShagieva <dariyashagieva@gmail.com>
2017-12-19 23:12:02 +0300

    add avtomat

---
+++ homework/avtomar/src/ru/itis/ex4.java
@@ -0,0 +1,42 @@
+package ru.itis;
+
+import java.util.Scanner;
+
+public class ex4 {
+    public static boolean goodWord(String str){
+        int state = 0;
+        boolean f = true;
+        int k = 1;
+        for (int i = 0; i < str.length() && f; i++) {
+            switch (state){
+                case 0:
+                    if (str.charAt(i) == '1') state = 1;
+                    break;
+                case 1:
+                    if (str.charAt(i) == '0') state = 2;
+                    break;
+                case 2:
+                    if (str.charAt(i) == '0') k++;
+                    else{
+                        if (k % 2 != 0) f = false;
+                        else {
+                            state = 1;
+                            k = 1;
+                        }
+                    }
+            }
+
+        }
+        return f;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        String[] str = s.split(" ");
+        for (int i = 0; i < str.length; i++) {
+            if (goodWord(str[i])) System.out.println(str[i] +" принято автоматом ");
+            else System.out.println(str[i] + " не принято автоматом" );
+
+        }
+    }
+}

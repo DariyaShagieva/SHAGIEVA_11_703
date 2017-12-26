7014f400705fdf5fe510bc4c45b48d1567ff2fc5
DariyaShagieva <dariyashagieva@gmail.com>
2017-12-26 19:41:38 +0300

    исправлен ex232

--- homework/src/ru/itis/stroki/ex232.java
+++ homework/src/ru/itis/stroki/ex232.java
@@ -6,16 +6,14 @@ public class ex232 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
-        int k = 0;
         boolean f;
         for (int i = 0; i < str.length(); i++) {
             f = true;
             for (int j = 0; j < str.length() && f  ; j++) {
                 if (i != j) if(str.charAt(i) == str.charAt(j)) f = false;
             }
-            if(f) k++;
+            if(f) System.out.print(str.charAt(i) + " ");
         }
-        System.out.println(k);   //where the symbols
 
     }
 }
